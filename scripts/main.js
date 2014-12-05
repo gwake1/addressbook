46e9650d099c40ab16b32e567e1b96a2e840396a
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-05 13:22:54 -0600

    ng-repeat complete

--- scripts/main.js
+++ scripts/main.js
@@ -12,5 +12,14 @@
       email: 'l335@aol.com'
     }
     ];
+
+    vm.newContact = function(){
+      vm.contacts.push(vm.newContact);
+    };
+
+    vm.removeContact = function(){
+      var index = vm.contacts.indexOf(contact);
+      vim.contacts.splice(index, 1);
+    };
   });
 })();
