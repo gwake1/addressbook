12de08b217506279ad8410ff3f24ab1bc28d82fc
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-08 15:51:30 -0600

    Partial Views working

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,35 @@
 ;(function(){
   'use strict';
   angular.module("myApp", ['ngRoute'])
-    .config(function($routeProvider){
-      $routeProvider
-      .when('/', {
-        templateUrl: 'views/table.html'
-      })
-      .when('/new', {
-        templateUrl: 'views/form.html'
-      })
-      .otherwise({redirectTo: '/'});
+  .config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl: 'views/table.html',
+      controller: 'ContactsController',
+      controllerAs: 'contactsCtrl'
     })
+    .when('/new', {
+      templateUrl: 'views/form.html',
+      controller: 'ContactsController',
+      controllerAs: 'contactsCtrl'
+    })
+    .when('/:id', {
+      templateUrl: 'views/show.html',
+      controller: 'ShowController',
+      controllerAs: 'showCtrl'
+    })
+    .otherwise({redirectTo: '/'});
+  })
+  .controller('ShowController', function($http, $routeParams){
+    var vm = this;
+    var id= $routeParams.id;
+    $http.get('https://geraldaddressbook.firebaseio.com/contacts/'+id+'.json')
+    .success(function(data){
+      vm.contact = data;
+    });
+  })
   .controller('ContactsController', function($http){
     var vm = this;
-
     var getData = function(){$http.get('https://geraldaddressbook.firebaseio.com/contacts.json')
     .success(function(data){
       vm.contacts = data;
