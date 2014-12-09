fa58a2b557512e9640332164f64c5d426773758b
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-09 11:17:08 -0600

    Boom

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,27 @@
 ;(function(){
   'use strict';
-  angular.module("myApp", ['ngRoute'])
+  angular.module("myApp", ['ngRoute', 'mgcrea.ngStrap'])
   .config(function($routeProvider){
     $routeProvider
     .when('/', {
       templateUrl: 'views/table.html',
       controller: 'ContactsController',
-      controllerAs: 'contactsCtrl'
+      controllerAs: 'modify'
     })
     .when('/new', {
       templateUrl: 'views/form.html',
       controller: 'ContactsController',
-      controllerAs: 'contactsCtrl'
+      controllerAs: 'modify'
     })
     .when('/:id', {
       templateUrl: 'views/show.html',
       controller: 'ShowController',
       controllerAs: 'showCtrl'
+    })
+    .when('/:id/edit', {
+      templateUrl: 'views/form.html',
+      controller: 'EditController',
+      controllerAs: 'modify'
     })
     .otherwise({redirectTo: '/'});
   })
@@ -26,33 +31,62 @@
     $http.get('https://geraldaddressbook.firebaseio.com/contacts/'+id+'.json')
     .success(function(data){
       vm.contact = data;
+    })
+    .error(function(err){
+      console.log(err)
     });
+  })
+  .controller('EditController', function($http, $routeParams, $location){
+    var vm = this;
+    var id= $routeParams.id;
+    var url= 'https://geraldaddressbook.firebaseio.com/contacts/'+id+'.json';
+    $http.get(url)
+    .success(function(data){
+      vm.newContact = data;
+    })
+    .error(function(err){
+      console.log(err)
+    });
+    vm.addNewContact = function(){
+      $http.put(url, vm.newContact)
+      .success(function(data){
+        $location.path('/');
+      })
+      .error(function(err){
+        console.log(err)
+      });
+    };
   })
   .controller('ContactsController', function($http){
     var vm = this;
-    var getData = function(){$http.get('https://geraldaddressbook.firebaseio.com/contacts.json')
+
+    $http.get('https://geraldaddressbook.firebaseio.com/contacts.json')
     .success(function(data){
       vm.contacts = data;
-    })};
-
-    getData();
+    })
+    .error(function(err){
+      console.log(err);
+    });
 
     vm.addNewContact = function(){
       $http.post('https://geraldaddressbook.firebaseio.com/contacts.json', vm.newContact)
       .success(function(data){
-        var successContact = data.name;
         vm.contacts[data.name] = vm.newContact;
         vm.newContact = freshContact();
-        $location.path(successContact);
+      })
+      .error(function(err){
+        console.log(err);
       });
     };
 
     vm.removeContact = function(contactId){
       var url = 'https://geraldaddressbook.firebaseio.com/contacts/' + contactId + '.json';
-      console.log(contactId);
       $http.delete(url)
       .success(function(data){
         delete vm.contacts[contactId];
+      })
+      .error(function(err){
+        console.log(err);
       });
     };
 
